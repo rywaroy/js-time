af10def61f11550840c73d0fd7f14fa4a7477c6d
zhangzhihao <464485682@qq.com>
2018-08-29 17:23:49 +0800

    Delete

--- src/test.js
+++
@@ -1,7 +0,0 @@
-import time from '../dist/bundle.js';
-// import time from '../dist/main';
-// const time = require('./dist/index');
-
-// console.log(time('2018-06-06T13:36:13.000Z').toDate());
-
-console.log(time('2018-07-10T01:44:57.556+0000').valueOf());
